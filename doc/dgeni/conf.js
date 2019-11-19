411a831e3e9d59919b6f919a6e95c3a454690c44
Emil Scoffone <emil@scoffone.com>
2019-11-18 16:25:13 -0800

    Added base documentation drivers

---
+++ doc/dgeni/conf.js
@@ -0,0 +1,79 @@
+// Canonical path provides a consistent path (i.e. always forward slashes) across different OSes
+var path = require('canonical-path');
+var Package = require('dgeni').Package;
+
+const JSDOCS = 'public/doc/fl/core/js';
+
+const STATE_ROOT = 'docs';
+const URL_ROOT = '/docs';
+const ENABLE_HTML5_MODE = 'no';
+const DEFAULT_PATH = '/api/index';
+const BRAND = 'Fl Core';
+const TITLE = 'Fl Core Documentation';
+
+module.exports = new Package('fl_core_docs', [
+    require('fl-dgeni/ngdoc')
+])
+
+// control debug dumps
+    .config(function(templateEngine) {
+	templateEngine.config.globals.debugDump = false;
+    })
+
+// use relative links
+// this is probably not needed because the documentation uses the @sref inline tag instead
+    .config(function(getLinkInfo) {
+	getLinkInfo.relativeLinks = true;
+    })
+
+// register external links (links to home pages for external packages)
+    .config(function(externalLinks) {
+	const XLINKS = [
+	    //		    { name: '', xlink: '' },
+
+	    { name: 'Axios', xlink: 'https://github.com/axios/axios' },
+	    { name: 'Rails-nested-resources',
+	      xlink: 'https://guides.rubyonrails.org/routing.html#nested-resources' },
+	    { name: 'Webpack', xlink: 'https://webpack.js.org' }
+	];
+
+	externalLinks.register(XLINKS);
+    })
+
+// sources, output location
+    .config(function(log, readFilesProcessor, writeFilesProcessor) {
+	log.level = 'info';
+
+	readFilesProcessor.basePath = path.resolve(__dirname, '../..');
+	readFilesProcessor.sourceFiles = [
+	    { include: 'app/assets/javascripts/fl/core/*.js', basePath: 'app/assets/javascripts' },
+
+	    { include: 'doc/dgeni/content/**/*.md', 
+	      basePath: 'doc/dgeni/content', fileReader: 'ngdocFileReader' }
+	];
+
+	writeFilesProcessor.outputFolder  = JSDOCS;
+    })
+
+// index page configuration
+    .config(function(indexPageProcessor) {
+	indexPageProcessor.brand = BRAND;
+	indexPageProcessor.navbar = [
+	    { id: 'content.api.main', label: 'API Reference' },
+	    { id: 'content.guide.main', label: 'Guide' }
+	];
+    })
+
+// app roots and configuration
+    .config(function(appModuleScriptProcessor, computeAngularStateProcessor, templateEngine) {
+	computeAngularStateProcessor.state_root = STATE_ROOT;
+
+	templateEngine.config.globals.state_root = STATE_ROOT;
+	templateEngine.config.globals.url_root = URL_ROOT;
+	templateEngine.config.globals.enable_html5_mode = ENABLE_HTML5_MODE;
+	templateEngine.config.globals.default_path = DEFAULT_PATH;
+	templateEngine.config.globals.brand = BRAND;
+	templateEngine.config.globals.title = TITLE;
+})
+
+;
---
+++ scripts/docs_ruby.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+function shell_session_update {
+    local X
+}
+
+if test "$(type -t yard)" = '' ; then
+    echo "Please install the Yard document generator (gem install yard)."
+    echo "See https://yardoc.org/"
+    exit 1
+fi
+
+if test "x$1" = "x" ; then
+    echo "usage: $0 [doc|stats]"
+    exit 1
+else
+    OP=$1
+fi
+
+CFGFILE="doc/ruby/yard.cfg"
+
+ORIGINALCWD=$(pwd)
+while test ! -f $CFGFILE ; do
+    OLDCWD=$(pwd)
+    cd ..
+    if test "x$(pwd)" = "x$OLDCWD" ; then
+	echo "$ORIGINALCWD does not seem to be in a fl-core distribution"
+	exit 1
+    fi
+done
+
+RUBYDOCS="public/doc/fl/core/ruby"
+
+case $OP in
+    doc) echo "generating documentation in $RUBYDOCS"
+	 rm -rf $RUBYDOCS
+
+	 if yard doc --yardopts $CFGFILE --output-dir=$RUBYDOCS ; then
+	     echo "rebuilt the Ruby documentation"
+	 else
+	     echo "failed to rebuild the Ruby docs"
+	     exit 1
+	 fi
+	 ;;
+
+    stats) echo "generating statistics (including undocumented items)"
+	   if yard stats --yardopts $CFGFILE --list-undoc ; then
+	       echo "generated statistics"
+	   else
+	       echo "failed to generate statistics"
+	       exit 1
+	   fi
+	   ;;
+    *) echo "unknown operation: $OP"
+       exit 1
+       ;;
+esac
+
+exit 0
