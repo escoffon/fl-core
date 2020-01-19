8600e9465c69ca899fa546193ff00360ce5bb9ca
Emil Scoffone <emil@scoffone.com>
2020-01-18 21:20:15 -0800

    Removed two code files we don't need or support

--- lib/fl/core/db/application_record.rb
+++
@@ -1,7 +0,0 @@
-module Fl::Core
-  # Base class for framework ActiveRecord objects.
-
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
--- lib/fl/core/db/neo4j.rb
+++
@@ -1,9 +0,0 @@
-module Fl::Core
-  # Namespace for Neo4j classes.
-
-  module Neo4j
-  end
-end
-
-#require 'fl/core/neo4j/rel'
-#require 'fl/core/neo4j/association_proxy_cache'
