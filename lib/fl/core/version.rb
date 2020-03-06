541f01b857bb207492da7e83f297f9b5d4260fcb
Emil Scoffone <emil@scoffone.com>
2020-03-06 02:38:28 -0800

    Bumped version

--- lib/fl/core/version.rb
+++ lib/fl/core/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Core
     # The current version number for the `fl-core` gem.
     
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
 
     # The version date for the fl-core gem.
 
-    DATE = '2020-02-28'
+    DATE = '2020-03-06'
   end
 end
