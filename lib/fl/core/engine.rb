49f0151a10f86b6ebdd655f28f92f629c1ebac35
Emil Scoffone <emil@scoffone.com>
2020-12-18 15:14:31 -0800

    commented out a stubbed engine initializer

--- lib/fl/core/engine.rb
+++ lib/fl/core/engine.rb
@@ -5,8 +5,8 @@ module Fl
     class Engine < ::Rails::Engine
       isolate_namespace Fl::Core
 
-      initializer "fl-core.test_initializer" do
-      end
+#      initializer "fl-core.initializer", before: :load_config_initializers do
+#      end
     end
   end
 end
