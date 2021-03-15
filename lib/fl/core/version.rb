526afa8cc5fc5ffe566d3632d6080225c2d0852b
Emil Scoffone <emil@scoffone.com>
2021-03-15 13:29:47 -0700

    bumped Axior minimum version in package.json to address a security alert

--- lib/fl/core/version.rb
+++ lib/fl/core/version.rb
@@ -2,7 +2,7 @@ module Fl
   module Core
     # The current version number for the `fl-core` gem.
     
-    VERSION = "0.5.9"
+    VERSION = "0.5.10"
 
     # The version date for the fl-core gem.
 
