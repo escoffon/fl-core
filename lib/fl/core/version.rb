a3d00fbf66c0d8dc1cce5464fb944afa69c2f30d
Emil Scoffone <emil@scoffone.com>
2021-04-30 17:36:30 -0700

    bumped version number

--- lib/fl/core/version.rb
+++ lib/fl/core/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Core
     # The current version number for the `fl-core` gem.
     
-    VERSION = "0.7.0"
+    VERSION = "0.7.1"
 
     # The version date for the fl-core gem.
 
-    DATE = '2021-04-29'
+    DATE = '2021-04-30'
   end
 end
