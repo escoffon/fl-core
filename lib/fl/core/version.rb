a0e532ca745128c9aaf1e8c0fac5759c939f5124
Emil Scoffone <emil@scoffone.com>
2022-02-07 13:32:38 -0800

    bumped version number to 0.7.13, bypassing the incorrect 0.7.12 tag

--- lib/fl/core/version.rb
+++ lib/fl/core/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Core
     # The current version number for the `fl-core` gem.
     
-    VERSION = "0.7.11"
+    VERSION = "0.7.13"
 
     # The version date for the fl-core gem.
 
-    DATE = '2021-08-08'
+    DATE = '2022-02-07'
   end
 end
