acd9be0225ddb423826105ac5c8b643fd44af21f
Emil Scoffone <emil@scoffone.com>
2024-03-08 15:09:26 -0800

    fixed a typo in the version date string

--- lib/fl/core/version.rb
+++ lib/fl/core/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Core
     # The current version number for the `fl-core` gem.
     
-    VERSION = "0.10.5"
+    VERSION = "0.10.6"
 
     # The version date for the fl-core gem.
 
-    DATE = '2024-03-8'
+    DATE = '2024-03-08'
   end
 end
